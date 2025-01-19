93a2470aa070c3606327ea6373d01fca76f1f41b
Nikita Galkin <nikiprostoff@gmail.com>
2025-01-19 14:27:50 +0500

    Добавил доску

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)'
+			},
+			gridTemplateColumns: {
+				'8-tiles': 'repeat(8, var(--tile-size))'
+			},
+			gridTemplateRows: {
+				'8-tiles': 'repeat(8, var(--tile-size))'
+			}
+			// backgroundColor: {
+			// 	'light-tile': 'var(--light-tile)',
+			// 	'dark-tile': 'var(--dark-tile)'
+			// }
+		}
+	},
+	plugins: []
+} satisfies Config
